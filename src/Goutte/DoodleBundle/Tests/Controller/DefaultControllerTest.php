dcc0453cfa29e340bbe062a9c9bb9cfd47b65f7b
Goutte <antoine.goutenoir@gmail.com>
2012-06-05 05:17:22 +0200

    Some tests : / and save

--- src/Goutte/DoodleBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Goutte/DoodleBundle/Tests/Controller/DefaultControllerTest.php
@@ -8,6 +8,22 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        // no tests here T_T
+
+//        rm -rf app/cache/*
+//        rm -rf app/logs/*
+//
+//        sudo chmod +a "www-data allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
+//        sudo chmod +a "`whoami` allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
+
+        $cleanup = array(
+            "php console cache:clear -e dev",
+            "php console cache:clear -e prod",
+        );
+
+
+        // Doodle on me page
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
