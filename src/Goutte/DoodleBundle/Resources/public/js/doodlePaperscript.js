ab65f0c18fdb9d71c6df992b926273bfd6cea49b
Goutte <antoine.goutenoir@gmail.com>
2012-06-16 00:13:59 +0200

    Fixing an issue with the right-click

--- src/Goutte/DoodleBundle/Resources/public/js/doodlePaperscript.js
+++ src/Goutte/DoodleBundle/Resources/public/js/doodlePaperscript.js
@@ -20,13 +20,16 @@ drawingTool.minDistance = minDistBetweenPoints;
 drawingTool.onMouseDown = function (event) {
   // If we produced a path before, deselect it:
   if (drawnPath) {
+    //drawnPath.selected = false;
+    drawingTool.onMouseUp(event);
+  } else {
+    // Create a new path
+    drawnPath = new Path();
+    drawnPath.add(event.point);
     drawnPath.selected = false;
+    drawnPath.strokeColor = 'white';
   }
 
-  // Create a new path
-  drawnPath = new Path();
-  drawnPath.add(event.point);
-  drawnPath.strokeColor = 'white';
 };
 
 // While the user drags the mouse or the finger, points are added to the path
@@ -69,6 +72,7 @@ drawingTool.onMouseUp = function (event) {
 
   var drawnPathCopy = addPathToHolder(drawnPath);
   drawnPath.remove();
+  drawnPath = null;
 
   // Add to the stack
   drawnPaths.push(drawnPathCopy);
@@ -100,159 +104,9 @@ function onFrame (event) {
 
 
 
-/** CONTROL LOGIC *****************************************************************************************************/
-
-function undo () {
-  var p = drawnPaths.pop();
-  p.remove();
-  updateControls('undo', {});
-  drawHolder();
-}
 
 
-function save () {
 
-  var saveRequest = new Request.JSON({
-    url: 'doodle/save',
-    method: 'post',
-    onRequest: function () {
-      log('Saving Doodle...');
-    },
-    onSuccess: function (responseJSON, responseText) {
-      log('Success !', responseText);
-      if (responseJSON.status == 'ok') {
-        updateControls('save', {doodleId: responseJSON.id});
-        //document.location.href = 'doodle/view/' + responseJSON.id;
-      } else if (responseJSON.status == 'error') {
-        notif(responseJSON.error);
-      }
-    },
-    onFailure: function () {
-      log('Fail ! Sorry.');
-      notif('Something went terribly wrong. Try again later ?');
-    }
-  });
-
-  var dataURL = canvasToImage(getHoldingCanvas(), '#000');
-
-  var img = document.createElement('img');
-  img.setAttribute('src', dataURL);
-
-  saveRequest.send(Object.toQueryString({
-    dataURL: dataURL
-  }));
-
-}
-
-
-function send (data) {
-
-  var sendRequest = new Request.JSON({
-    url: 'doodle/send/' + data.id,
-    method: 'post',
-    onRequest: function () {
-      log('Sending Doodle...');
-    },
-    onSuccess: function (responseJSON, responseText) {
-      log('Success !', responseText);
-      if (responseJSON.status == 'ok') {
-        updateControls('send', data);
-      } else if (responseJSON.status == 'error') {
-        notif(responseJSON.error);
-      }
-    },
-    onFailure: function () {
-      log('Fail ! Sorry.');
-      notif('Something went terribly wrong. Try again later ?');
-    }
-  });
-
-  sendRequest.send(Object.toQueryString(data));
-
-}
-
-function getLinkSend (doodleId) {
-  return 'doodle/send/' + doodleId;
-}
-
-function getLinkDownAsImage (doodleId) {
-  return 'doodle/download/' + doodleId;
-}
-
-function getLinkViewAsImage (doodleId) {
-  return 'doodle/view/' + doodleId;
-}
-
-
-/** CONTROLS **********************************************************************************************************/
-
-function updateControls (from, options) {
-  var buttonSave = document.id('buttonSave');
-  var buttonUndo = document.id('buttonUndo');
-  var buttonSend = document.id('buttonSend');
-  var buttonView = document.id('buttonView');
-  var buttonDown = document.id('buttonDown');
-  var formSend = document.id('formSend');
-
-  // Show / Hide Save & Undo
-  if ('save' != from && 'send' != from && drawnPaths.length) {
-    buttonSave.removeClass('hiddenSmall');
-    buttonUndo.removeClass('hiddenSmall');
-  } else {
-    buttonSave.addClass('hiddenSmall');
-    buttonUndo.addClass('hiddenSmall');
-  }
-
-  // Show / Hide Download & Send & View
-  if ('save' == from && options.doodleId) {
-    buttonSend.setAttribute('href', getLinkSend(options.doodleId));
-    buttonSend.setAttribute('doodleId', options.doodleId);
-    buttonSend.removeClass('hiddenSmall');
-    buttonView.setAttribute('href', getLinkViewAsImage(options.doodleId));
-    buttonView.removeClass('hiddenSmall');
-    buttonDown.setAttribute('href', getLinkDownAsImage(options.doodleId));
-    buttonDown.removeClass('hiddenSmall');
-    notif('<b>Your doodle has been saved.</b><br />' +
-          'You can send it to me along with a message, ' +
-          'view the image in a new tab or simply download it as a png image.');
-  }
-
-  if ('draw' == from) {
-    if (drawnPaths.length == 1) {
-      notif('Good job ! Have fun !', {clear:true});
-    } else if (drawnPaths.length == 6) {
-      notif('<b>KEYBOARD ENABLED !</b><br />You can hit <b>[z]</b> to undo your last draw.');
-    } else if (drawnPaths.length == 12) {
-      notif('The page is probably getting slower.<br />It is expected -- this is a performance experiment.');
-    } else if (drawnPaths.length == 32) {
-      notif("Just hit <b>[c]</b> for Free Cake™ !");
-    } else if (drawnPaths.length == 100) {
-      notif("Waow, that's a big doodle you're drawing there !<br />I hope you'll save that !");
-    } else if (drawnPaths.length == 256) {
-      notif("<b>~ ACHIEVEMENT UNLOCKED ~</b><br />Web Doodle Artist");
-    }
-    // Hide control buttons
-    buttonSend.addClass('hiddenSmall');
-    buttonView.addClass('hiddenSmall');
-    buttonDown.addClass('hiddenSmall');
-    formSend.addClass('hiddenSmall');
-  }
-
-  if ('send' == from) {
-    if (!options.title && !options.message) {
-      buttonSend.addClass('used');
-      formSend.removeClass('hiddenSmall');
-    } else {
-      warpDoodleIntoSpace();
-    }
-  }
-}
-
-
-function warpDoodleIntoSpace () {
-  document.id('formSend').addClass('hiddenSmall');
-  notifs.add("Well done, and thank you !");
-}
 
 /** DOM BEHAVIORS *****************************************************************************************************/
 
@@ -384,47 +238,6 @@ function getHoldingCanvas () {
 }
 
 
-/**
- * Returns the dataURL (Base64 encoded data url string)
- * of the specified canvas, but applies background color first
- * @param canvas
- * @param backgroundColor
- * @return {String} the dataURL
- */
-function canvasToImage (canvas, backgroundColor) {
-  // cache height and width
-  var w = canvas.width;
-  var h = canvas.height;
-  var context = canvas.getContext("2d");
-  var data;
-
-  if (backgroundColor) {
-    // get the current ImageData for the canvas.
-    data = context.getImageData(0, 0, w, h);
-    // store the current globalCompositeOperation
-    var compositeOperation = context.globalCompositeOperation;
-    // set to draw behind current content
-    context.globalCompositeOperation = "destination-over";
-    // set background color
-    context.fillStyle = backgroundColor;
-    // draw background / rect on entire canvas
-    context.fillRect(0, 0, w, h);
-  }
-
-  // get the image data from the canvas
-  var imageData = canvas.toDataURL("image/png");
-
-  if (backgroundColor) {
-    // clear the canvas
-    context.clearRect(0, 0, w, h);
-    // restore it with original / cached ImageData
-    context.putImageData(data, 0, 0);
-    // reset the globalCompositeOperation to what it was
-    context.globalCompositeOperation = compositeOperation;
-  }
-
-  return imageData;
-}
 
 // see https://github.com/paperjs/paper.js/issues/48
 //// These did not match up for some reason
