2d52b6d45e50aac4c75572cb5976ff856ea5f786
Goutte <antoine@goutenoir.com>
2023-04-24 10:08:29 +0200

    Clean up

--- src/Controller/DrawingController.php
+++ src/Controller/DrawingController.php
@@ -14,6 +14,14 @@ use Symfony\Component\Yaml\Yaml;
 
 class DrawingController extends AbstractController
 {
+
+    // TODO: move to another controller
+    #[Route(path: '/ping', name: 'app_ping')]
+    public function ping(): Response
+    {
+        return new Response("pong");
+    }
+
     #[Route(path: '/', name: 'app_drawing_draw')]
     public function draw(): Response
     {
